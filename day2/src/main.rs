15c02a03699c02bd111258269f8c1040ab3d0c72
David Marchante <davidmarchan@gmail.com>
2026-01-06 23:35:47 -0500

    day2p1

---
+++ day2/src/main.rs
@@ -0,0 +1,66 @@
+use day2::parse;
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    println!("p1 = {:?}", p1(input));
+    println!("p2 = {:?}", p2(input));
+}
+
+fn p1(input: &str) -> u64 {
+    parse(input)
+        .iter()
+        .flat_map(|range| range.clone().filter(|&a| !is_valid_id(a)))
+        .sum()
+}
+
+fn is_valid_id(id: u64) -> bool {
+    let binding = id.to_string();
+    let id = binding.as_bytes();
+
+    if !id.len().is_multiple_of(2) {
+        return true;
+    }
+
+    let first_half = 0..id.len() / 2;
+    let second_half = id.len() / 2..id.len();
+
+    for (i, j) in first_half.clone().zip(second_half) {
+        if id[i] != id[j] {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn p2(input: &str) -> usize {
+    123
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    fn p1_test() {
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+        assert_eq!(p1(input), 1227775554);
+    }
+
+    #[test]
+    fn p2_test() {
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+
+        // assert_eq!(p2(input), 23);
+    }
+
+    #[test]
+    fn is_valid_id_test() {
+        assert_eq!(is_valid_id(123123), false);
+        assert_eq!(is_valid_id(101), true);
+        assert_eq!(is_valid_id(1188511880), true);
+    }
+}
---
+++ day2/src/parser.rs
@@ -0,0 +1,40 @@
+use std::ops::RangeInclusive;
+
+use nom::combinator::all_consuming;
+use nom::multi::separated_list1;
+use nom::{
+    IResult, Parser, bytes::complete::tag, character::complete::u64, combinator::map,
+    sequence::separated_pair,
+};
+
+pub fn parse(input: &str) -> Vec<RangeInclusive<u64>> {
+    let (_rest, ranges) = separated_list1(tag(","), parse_range).parse(input).unwrap();
+
+    ranges
+}
+
+fn parse_range(input: &str) -> IResult<&str, RangeInclusive<u64>> {
+    map(separated_pair(u64, tag("-"), u64), |(a, b)| a..=b).parse(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_range_test() {
+        assert_eq!(parse_range("11-22"), Ok(("", 11..=22)));
+        assert_eq!(
+            parse_range("1188511880-1188511890"),
+            Ok(("", 1188511880..=1188511890))
+        );
+    }
+
+    #[test]
+    fn parse_test() {
+        assert_eq!(
+            parse("11-22,95-115,998-1012"),
+            vec![11..=22, 95..=115, 998..=1012]
+        );
+    }
+}
