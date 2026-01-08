6527776afbd6d9a8dca8520475ec4f074ecf903a
David Marchante <davidmarchan@gmail.com>
2026-01-07 19:19:25 -0500

    day3p2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,9 +4,10 @@ fn main() {
     let input = include_str!("input.txt");
 
     println!("p1 = {:?}", p1(input));
-    // println!("p2 = {:?}", p2(input));
+    println!("p2 = {:?}", p2(input));
 }
 
+// 2 batteries
 fn p1(input: &str) -> u64 {
     input
         .lines()
@@ -41,8 +42,57 @@ fn max_index(array: &[u8]) -> usize {
     idx
 }
 
+// 12 batteries
 fn p2(input: &str) -> u64 {
-    todo!()
+    input
+        .lines()
+        .map(|line| {
+            let line = line.as_bytes();
+            let mut indices = vec![];
+
+            max_recursive(line, 0, 12, &mut indices);
+
+            let binding = indices.iter().map(|&i| line[i]).collect::<Vec<u8>>();
+            let result = std::str::from_utf8(&binding).unwrap();
+
+            tracing::info!(line = %std::str::from_utf8(line).unwrap());
+            tracing::debug!(?indices);
+
+            result.parse::<u64>().unwrap()
+        })
+        .inspect(|x| tracing::debug!(result = x))
+        .sum()
+}
+
+// XXXXX XXXXX XXXXX XXXXX
+// ╰────────╯
+// 0 + (20 - (0+1)) - (12 - 1) = 8 => gotta leave a window of at least 11 characters
+//
+// XXXMX XXXXX XXXXX XXXXX
+//     ╰─────╯
+// if i = 3 => 16 spaces left for 11 digits (20 - (i+1))
+// 4 + (20 - (4+1)) - (11 - 1) = 9 => gotta leave a window of at least 10 characters
+//
+// end = start + (arr.len() - (start+1)) - (chars_left - 1) => till where to look for max char
+fn max_recursive(array: &[u8], start: usize, chars_left: usize, indices: &mut Vec<usize>) {
+    if chars_left == 0 {
+        return;
+    }
+
+    let end = start + (array.len() - (start + 1)) - (chars_left - 1);
+    let mut max_idx = 0;
+    let mut max_val = 0u8;
+
+    for i in start..=end {
+        if array[i] > max_val {
+            max_val = array[i];
+            max_idx = i;
+        }
+    }
+
+    indices.push(max_idx);
+
+    max_recursive(array, max_idx + 1, chars_left - 1, indices);
 }
 
 #[cfg(test)]
@@ -81,6 +131,41 @@ mod tests {
             818181911112111
         "};
 
-        // assert_eq!(p2(input), 357);
+        assert_eq!(p2(input), 3121910778619);
+    }
+
+    #[test]
+    fn max_recursive_test() {
+        let input = b"987654321111111";
+        let mut indices = vec![];
+        max_recursive(input, 0, 12, &mut indices);
+        let binding = indices.iter().map(|&i| input[i]).collect::<Vec<u8>>();
+        let result = std::str::from_utf8(&binding).unwrap();
+
+        assert_eq!(result, "987654321111");
+
+        let input = b"811111111111119";
+        let mut indices = vec![];
+        max_recursive(input, 0, 12, &mut indices);
+        let binding = indices.iter().map(|&i| input[i]).collect::<Vec<u8>>();
+        let result = std::str::from_utf8(&binding).unwrap();
+
+        assert_eq!(result, "811111111119");
+
+        let input = b"234234234234278";
+        let mut indices = vec![];
+        max_recursive(input, 0, 12, &mut indices);
+        let binding = indices.iter().map(|&i| input[i]).collect::<Vec<u8>>();
+        let result = std::str::from_utf8(&binding).unwrap();
+
+        assert_eq!(result, "434234234278");
+
+        let input = b"818181911112111";
+        let mut indices = vec![];
+        max_recursive(input, 0, 12, &mut indices);
+        let binding = indices.iter().map(|&i| input[i]).collect::<Vec<u8>>();
+        let result = std::str::from_utf8(&binding).unwrap();
+
+        assert_eq!(result, "888911112111");
     }
 }
