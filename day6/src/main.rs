4ecd964a91d8413d165d1379d10956c25ee67547
David Marchante <davidmarchan@gmail.com>
2026-01-29 23:47:27 -0500

    day6p2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -42,7 +42,76 @@ fn p1(input: &str) -> u64 {
 }
 
 fn p2(input: &str) -> u64 {
-    todo!()
+    let mut problems: Vec<Vec<u8>> = vec![];
+    let mut operators: Vec<u8> = vec![];
+
+    // parse problem
+    for line in input.lines() {
+        let line = line.as_bytes();
+        match line[0] {
+            b'*' | b'+' => operators = line.to_owned(),
+            _ => problems.push(line.to_owned()),
+        }
+    }
+
+    debug_line(&operators);
+    for problem in &problems {
+        debug_line(problem);
+    }
+
+    // walk operators line to define grids
+    let mut operator_indices: Vec<usize> = vec![];
+
+    for (i, char) in operators.iter().enumerate() {
+        match char {
+            b'*' | b'+' => operator_indices.push(i),
+            _ => (),
+        }
+    }
+
+    tracing::debug!(?operator_indices);
+
+    // calculate grids
+    let mut op_iter = operator_indices.into_iter().peekable();
+    let mut total = 0;
+
+    while let Some(start) = op_iter.next() {
+        let end = op_iter.peek().copied().unwrap_or(operators.len() + 1) - 1;
+
+        let operation = operators[start];
+        let mut numbers: Vec<u64> = vec![];
+
+        // parse every column
+        for j in start..end {
+            let mut number: Vec<u8> = vec![];
+
+            for i in 0..problems.len() {
+                if problems[i][j] != b' ' {
+                    number.push(problems[i][j]);
+                }
+            }
+
+            debug_line(&number);
+            let number = std::str::from_utf8(&number).unwrap().parse().unwrap();
+            numbers.push(number);
+        }
+
+        tracing::debug!(?numbers);
+
+        total += match operation {
+            b'*' => numbers.iter().product::<u64>(),
+            b'+' => numbers.iter().sum(),
+            _ => panic!("keep calm!"),
+        };
+
+        tracing::debug!(?total);
+    }
+
+    total
+}
+
+fn debug_line(line: &[u8]) {
+    tracing::debug!("{}", std::str::from_utf8(line).unwrap());
 }
 
 #[cfg(test)]
@@ -50,28 +119,19 @@ mod tests {
     use super::*;
     use pretty_assertions::assert_eq;
 
+    const INPUT: &str = include_str!("input_test.txt");
+
     #[test]
     fn p1_test() {
         tracing_subscriber::fmt::init();
-        let input = indoc::indoc! {"
-            123 328  51 64
-            45 64  387 23
-            6 98  215 314
-            *   +   *   +
-        "};
 
-        assert_eq!(p1(input), 4277556);
+        assert_eq!(p1(INPUT), 4277556);
     }
 
     #[test]
     fn p2_test() {
-        let input = indoc::indoc! {"
-            123 328  51 64
-            45 64  387 23
-            6 98  215 314
-            *   +   *   +
-        "};
+        tracing_subscriber::fmt::init();
 
-        // assert_eq!(p2(input), 4277556);
+        assert_eq!(p2(INPUT), 3263827);
     }
 }
