d61be9108a98aa2fd9f76678aa1e62821e02aced
David Marchante <davidmarchan@gmail.com>
2026-02-01 00:15:45 -0500

    day7p2

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     tracing_subscriber::fmt::init();
 
@@ -58,7 +60,59 @@ fn p1(input: &str) -> u64 {
 }
 
 fn p2(input: &str) -> u64 {
-    todo!()
+    let grid: Vec<Vec<u8>> = input
+        .lines()
+        .map(|line| line.as_bytes().to_owned())
+        .collect();
+
+    // 1. keep a cursor of the current pointer
+    // 2. traverse down like if it were a tree with DFS, keeping track of each time it goes
+    //    left and right, calling the traversing function recursively
+    // 3. add 1 each time you reach the end, returning from the function
+    //
+    // NOTE: keep a memo to not recompute solved problems
+    let start: (usize, usize) = (0, grid[0].iter().position(|&x| x == b'S').unwrap());
+    let mut memo = HashMap::new();
+    traverse_grid(&grid, start, &mut memo)
+}
+
+fn traverse_grid(
+    grid: &[Vec<u8>],
+    start: (usize, usize),
+    memo: &mut HashMap<(usize, usize), u64>,
+) -> u64 {
+    if let Some(&result) = memo.get(&start) {
+        return result;
+    }
+
+    let (i, j) = start;
+    let mut timelines = 0;
+
+    tracing::debug!("{:?}", (i, j));
+    if i == grid.len() - 1 {
+        memo.insert(start, 1);
+        return 1;
+    }
+
+    match grid[i + 1][j] {
+        b'^' => {
+            // go left
+            if j > 0 {
+                timelines += traverse_grid(grid, (i + 1, j - 1), memo);
+            }
+
+            // go right
+            if j < grid[i].len() - 1 {
+                timelines += traverse_grid(grid, (i + 1, j + 1), memo);
+            }
+        }
+        _ => {
+            timelines += traverse_grid(grid, (i + 1, j), memo);
+        }
+    }
+
+    memo.insert(start, timelines);
+    timelines
 }
 
 fn debug_line(line: &[u8]) {
@@ -72,7 +126,7 @@ mod tests {
 
     #[test]
     fn p1_test() {
-        tracing_subscriber::fmt::init();
+        let _ = tracing_subscriber::fmt::try_init();
         let input = indoc::indoc! {"
             .......S.......
             ...............
@@ -96,9 +150,8 @@ mod tests {
     }
 
     #[test]
-    #[ignore = "pending"]
     fn p2_test() {
-        tracing_subscriber::fmt::init();
+        let _ = tracing_subscriber::fmt::try_init();
         let input = indoc::indoc! {"
             .......S.......
             ...............
@@ -118,6 +171,6 @@ mod tests {
             ...............
         "};
 
-        assert_eq!(p2(input), 21);
+        assert_eq!(p2(input), 40);
     }
 }
